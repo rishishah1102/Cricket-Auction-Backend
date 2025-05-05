ba3580a3f1a969976d01b3b4e9dfba40c53d42f8
rishishah1102 <rishi.shah545@gmail.com>
2025-05-05 22:39:33 +0530

    Allowed multiple origins in CORS

--- api/index.go
+++ api/index.go
@@ -17,7 +17,21 @@ var (
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "http://127.0.0.1:3000") // CORS
+		// Allowing multiple origins
+		origin := c.Request.Header.Get("Origin")
+		allowedOrigins := []string{
+			"http://localhost:3000",
+			"http://127.0.0.1:3000",
+			// TODO
+		}
+
+		for _, o := range allowedOrigins {
+			if origin == o {
+				c.Header("Access-Control-Allow-Origin", origin) // CORS
+				break
+			}
+		}
+
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE")
 		c.Header("Access-Control-Allow-Credentials", "true")
