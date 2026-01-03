80480b3b4854b02a8b3a32427e9db887e60062b0
rishishah1102 <rishi.shah545@gmail.com>
2026-01-03 22:56:45 +0530

    Vercel deployment

--- api/index.go
+++ api/index.go
@@ -31,7 +31,7 @@ func init() {
 	mongoConfig.MongoURI = os.Getenv("MONGODB_URI")
 	mongoConfig.DbName = os.Getenv("DATABASE_NAME")
 
-	client, err := database.NewMongoClient(ctx, mongoConfig.MongoURI, logger)
+	client, err := database.NewMongoClient(ctx, "mongodb+srv://rishishah545:rE0VFrEmuqN0WSbc@auction.xtxvgwg.mongodb.net/auction", logger)
 	if err != nil {
 		logger.Error("failed to create mongo client", zap.Any(constants.Err, err))
 	}
